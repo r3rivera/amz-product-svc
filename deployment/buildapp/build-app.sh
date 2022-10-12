5cb72d7643376a691a444a96a65dd8620accf656
r3rivera <r.ryan.rivera@outlook.com>
2022-10-12 10:20:35 -0500

    Added dockerfile template

--- deployment/buildapp/build-app.sh
+++ deployment/buildapp/build-app.sh
@@ -17,7 +17,7 @@ echo "Git Branch   :: ${GIT_BRANCH}"
 echo "Build ID     :: ${BUILD_ID}"
 echo "Build Number :: ${BUILD_NUMBER}"
 echo "Git Branch   :: ${GIT_BRANCH}"
-GIT_COMMITID="$(git log | head -n 1 | cut -d ' ' -f 2)"
+GIT_COMMITID="$(git log | head -n 1 | cut -w -f 2)"
 BUILD_DATE="$(date +%m%d%Y)"
 BUILD_TIME="$(date +%H%M%S)"
 echo "Build Date   :: ${BUILD_DATE}"
@@ -26,13 +26,14 @@ echo "Git Commit   :: ${GIT_COMMITID}"
 echo ""
 echo ""
 echo "Start compiling the application"
-mvn clean install
+mvn clean package
 JAR_FILE="$(find ./target/*.jar | head -n 1 | cut -d '/' -f 3)"
 echo "Compilation Complete :: JAR File is ${JAR_FILE}"
 echo ""
 echo ""
 echo "Start Building the Docker Image"
 docker --version
+
 
 
 
